f3d965afedcd93d0cae78005faf429b7654777d7
Mark Railton <mark.railton@outlook.ie>
2026-02-11 00:42:27 +0000

    Dont seed admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PermissionSeeder::class,
             RoleSeeder::class,
-            AdminUserSeeder::class,
         ]);
     }
 }
